29765bc496cc70899b966122e07bf8cbc26ee82a
Bentley <sbentley2@corpuk.net>
2024-12-20 09:51:34 +0000

    improve tests

--- app.py
+++ app.py
@@ -9,36 +9,11 @@ calendar_days_to_build = 30
 start_date = test_cases.TEST_date
 
 personal_calendar = personal_calendars.calendars(test_cases.TEST_events, start_date)
-to_dos, outgoings, total_outgoings = personal_calendar.create_output_calendar(personal_calendar.create_calendars(calendar_days_to_build=calendar_days_to_build))
+calendars  = personal_calendar.create_calendars(calendar_days_to_build=calendar_days_to_build)
+to_dos, outgoings, total_outgoings = personal_calendar.create_output_calendar(calendars)
 
 current_todos = personal_calendar.update_current_tasks(to_dos)
-
-# ren_personal_calendar = {}
-# ren_outgoing_calendar = {}
-# ren_total_outgoing_calendar = {}
-# dates = [(start_date + timedelta(days=i)) for i in range(calendar_days_to_build)]
-
-# for date in dates:
-
-#     if date in total_outgoings.keys():
-#         total_outgoing = total_outgoings[date]
-#     else:
-#         total_outgoing = 0
-
-#     if date in to_dos.keys():
-#         todo = to_dos[date]
-#     else:
-#         todo = ["None scheduled."]
-
-#     if date in outgoings.keys():
-#         outgoing = outgoings[date]
-#     else:
-#         outgoing = ["None scheduled."]
-
-#     ren_personal_calendar[date.strftime("%a %d %m")] =  todo
-#     ren_outgoing_calendar[date.strftime("%a %d %m")] =  outgoing
-#     ren_total_outgoing_calendar[date.strftime("%a %d %m")] =  total_outgoing
-                   
+                 
     
 @app.route('/')
 
--- test.py
+++ test.py
@@ -11,6 +11,7 @@ personal_calendar = personal_calendars.calendars(test_cases.TEST_events, start_d
 to_dos, outgoings, total_outgoings = personal_calendar.create_output_calendar(personal_calendar.create_calendars(calendar_days_to_build=calendar_days_to_build))
 
 test_key = start_date.strftime("%a %d %m")
+test_key_2 = date(2024,12,2).strftime("%a %d %m")
 
 assert len(total_outgoings) == calendar_days_to_build
 assert len(to_dos) == calendar_days_to_build
@@ -19,10 +20,15 @@ assert len(outgoings) == calendar_days_to_build
 assert to_dos[test_key] == ["Drink water"]
 assert outgoings[test_key] == ["None Scheduled."]
 assert total_outgoings[test_key] == 0
-assert total_outgoings[date(2024,12,1).strftime("%a %d %m")] == 0
+
+assert total_outgoings[test_key_2] == 1277 + 150.91
+assert outgoings[test_key_2] == [] # get expected
+assert to_dos[test_key_2] == [] # get expected
 
 start_date = date.today()
 current_tasks = personal_calendar.update_current_tasks(test_cases.TEST_outstanding_actions)
+
+# current tasks includes new from current day and test cases outstanding.
 
 
 
