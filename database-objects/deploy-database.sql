8037ce0bf1f227972a70f659dc11a92c3380e31a
bentley <stevenjbentley@gmail.com>
2024-12-22 22:31:47 +0000

    tables for test data

---
+++ database-objects/deploy-database.sql
@@ -0,0 +1,63 @@
+create schema task_catalog;
+
+create  table TASK_CATALOG.TASKS
+(
+    ID BIGINT IDENTITY(1, 1),
+    CLASSIFYING_ID VARCHAR(255),
+    name varchar(255) not null,
+    description text,
+    created_at datetime default CURRENT_TIMESTAMP,
+    lastupdated_at datetime, 
+    type varchar(255) not null,
+
+    repeate_type VARCHAR(255),
+    dayofmonth integer,
+    dayofweek integer,
+    timeofday time,
+    numberofperiods integer,
+
+    amount decimal,
+    account VARCHAR(255),
+
+    weekdays_only TINYINT DEFAULT 0,
+    max_concurrent integer DEFAULT 1,
+    PERSISTED TINYINT DEFAULT 0
+)
+;
+
+CREATE UNIQUE INDEX task_catalog_tasks_id_uindex ON task_catalog.tasks (ID);
+
+CREATE TABLE task_catalog.TASK_INSTANCES
+(
+    ID BIGINT IDENTITY(1, 1),
+    TASK_ID BIGINT,
+    STATUS VARCHAR(255),
+    CREATED DATETIME default CURRENT_TIMESTAMP, 
+    FINISHED_AT DATETIME,
+    LAST_UPDATED_AT DATETIME,
+
+);
+
+CREATE UNIQUE INDEX task_catalog_task_instances_id_uindex ON task_catalog.task_instances (ID);
+;
+
+ALTER TABLE task_catalog.TASK_INSTANCES
+ADD CONSTRAINT FK_TASKS_TASTKCATALOG FOREIGN KEY (TASK_ID)
+REFERENCES task_catalog.TASKS (ID);
+
+;
+
+CREATE TABLE task_catalog.CURRENT_TASKS
+(
+    ID BIGINT IDENTITY(1, 1),
+    TASK_ID BIGINT,
+    TASK_INSTANCE_ID BIGINT,
+    STATUS VARCHAR(255),
+    CREATED DATETIME default CURRENT_TIMESTAMP, 
+    FINISHED_AT DATETIME,
+    LAST_UPDATED_AT DATETIME,
+    CONSTRAINT FK_CURRENTTASKS_TASTKCATALOG FOREIGN KEY (TASK_ID)
+    REFERENCES task_catalog.TASKS (ID),
+    CONSTRAINT FK_CURRENTTASK_TASKINSTANCE FOREIGN KEY (TASK_INSTANCE_ID)
+    REFERENCES task_catalog.TASK_INSTANCES (ID)
+);
